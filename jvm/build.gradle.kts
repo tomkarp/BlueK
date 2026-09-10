411ad8699f9f5088c20f3e679becba58a1057e07
Thomas Karp <karpthomas@gmail.com>
2026-09-10 12:16:01 +0200

    Build worker through pinned Gradle wrapper

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,4 +1,16 @@
-plugins { kotlin("jvm") version "2.2.21" }
+import org.gradle.api.file.DuplicatesStrategy
+
+plugins {
+    kotlin("jvm") version "2.2.21"
+    application
+}
 group = "de.tomkarp.bluek"
 version = "0.1.0"
 kotlin { jvmToolchain(21) }
+application { mainClass.set("de.tomkarp.bluek.WorkerKt") }
+
+tasks.jar {
+    manifest { attributes["Main-Class"] = "de.tomkarp.bluek.WorkerKt" }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
